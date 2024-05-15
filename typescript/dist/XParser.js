343d73a0d581599452098967b1bb8ea66461b9ed
Roderick Rocamora <ric2xroc@gmail.com>
2024-05-16 04:35:56 +0800

    remove dist directory

--- typescript/dist/XParser.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-module.exports = class XParser {
-    constructor(url) {
-        this.url = url;
-    }
-    async fetchData() {
-    }
-};
